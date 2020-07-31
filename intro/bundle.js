30a256fc32dcbe1b07d5906b74aecb21378dac0b
Alikhan Bulatov <ali-bulatov@protonmail.com>
2020-07-31 16:03:06 -0400

    Intro d3.js created

---
+++ intro/bundle.js
@@ -0,0 +1,9 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}((function () { 'use strict';
+
+	const svg = d3.select('svg');
+	svg.style('background-color','red');
+
+})));
